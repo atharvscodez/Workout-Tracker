92618af193a9a165020a4957ae8f281cc4a141ab
Atharv Malhotra <atharvmalhotra@outlook.com>
2026-06-01 16:30:41 -0500

    add firebase env variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNbnrYeogHANxJAm83SU4yGgLey1dA4Js",
-  authDomain: "workout-tracker-f205a.firebaseapp.com",
-  projectId: "workout-tracker-f205a",
-  storageBucket: "workout-tracker-f205a.firebasestorage.app",
-  messagingSenderId: "566582969275",
-  appId: "1:566582969275:web:ab2e28f1864b647b24ba9e",
-  measurementId: "G-YVPZ2E8TBG",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
